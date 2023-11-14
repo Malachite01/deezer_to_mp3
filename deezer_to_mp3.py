61880c2ea1adbaf8482c8fb5c95b42d4c276eaa2
Mathieu Antunes <mathieu.antunes31@gmail.com>
2023-11-14 11:36:44 +0100

    No youtube API key

--- deezer_to_mp3.py
+++ deezer_to_mp3.py
@@ -1,14 +1,6 @@
 import requests
 import json
-import yaml
 import os
-from tqdm import tqdm
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-from pytube import YouTube
-from pytube.cli import on_progress
-from youtube_transcript_api import YouTubeTranscriptApi
-from pathlib import Path
 from yt_dlp import YoutubeDL
 
 
@@ -70,40 +62,102 @@ def playlist_to_json_file(OUTPUT_FILE, api_url, data):
 #endregion
 
 #region YoutubeAPI.py
-def get_creds():
-    with open("credentials.yaml", "r") as file:
-        creds = yaml.safe_load(file)
-    api_key = creds["api_key"]
-    return api_key
+YOUTUBE_ENDPOINT = "https://www.youtube.com"
 
-def get_video_listings(api_key, query):
+def get_youtube_init_data(url):
+    init_data = {}
+    api_token = None
+    context = None
     try:
-        youtube = build('youtube', 'v3', developerKey=api_key)
+        page = requests.get(url)
+        yt_init_data = page.text.split("var ytInitialData =")[1].split("</script>")[0].strip()[:-1]
 
-        search_response = youtube.search().list(
-            part='snippet',
-            type='video',
-            q=query,
-            maxResults=1
-        ).execute()
+        if "innertubeApiKey" in page.text:
+            api_token = page.text.split("innertubeApiKey")[1].split(",")[0].split('"')[2]
 
-        print(yaml.dump(search_response["items"]))
-        video_ids = list(
-            map(lambda x: x["id"]["videoId"], search_response["items"])
-        )
-        return video_ids
+        if "INNERTUBE_CONTEXT" in page.text:
+            context = json.loads(page.text.split("INNERTUBE_CONTEXT")[1].strip()[2:-2])
 
-    except HttpError as e:
-        print(f'An HTTP error {e.resp.status} occurred: {e.content}')
-        return []
+        init_data = json.loads(yt_init_data)
+        return {"initdata": init_data, "apiToken": api_token, "context": context}
+    except Exception as ex:
+        print(ex)
+        return {"initdata": init_data, "apiToken": api_token, "context": context}
 
-def form_youtube_url(video_id):
-    return "http://youtu.be/" + video_id
+def get_data(keyword, with_playlist=False, limit=0, options=None):
+    endpoint = f"{YOUTUBE_ENDPOINT}/results?search_query={keyword}"
+    try:
+        if options and isinstance(options, list) and len(options) > 0:
+            type_option = next((opt["type"] for opt in options if "type" in opt), None)
+            if isinstance(type_option, str):
+                if type_option.lower() == "video":
+                    endpoint += "&sp=EgIQAQ%3D%3D"
+                elif type_option.lower() == "channel":
+                    endpoint += "&sp=EgIQAg%3D%3D"
+                elif type_option.lower() == "playlist":
+                    endpoint += "&sp=EgIQAw%3D%3D"
+                elif type_option.lower() == "movie":
+                    endpoint += "&sp=EgIQBA%3D%3D"
 
-def download_mp3(video_id):
+        page = get_youtube_init_data(endpoint)
+
+        section_list_renderer = page["initdata"]["contents"]["twoColumnSearchResultsRenderer"]["primaryContents"]["sectionListRenderer"]
+
+        cont_token = {}
+        items = []
+
+        for content in section_list_renderer["contents"]:
+            if "continuationItemRenderer" in content:
+                cont_token = content["continuationItemRenderer"]["continuationEndpoint"]["continuationCommand"]["token"]
+            elif "itemSectionRenderer" in content:
+                for item in content["itemSectionRenderer"]["contents"]:
+                    if "channelRenderer" in item:
+                        channel_renderer = item["channelRenderer"]
+                        items.append({
+                            "id": channel_renderer["channelId"],
+                            "type": "channel",
+                            "thumbnail": channel_renderer["thumbnail"],
+                            "title": channel_renderer["title"]["simpleText"]
+                        })
+                    else:
+                        video_render = item.get("videoRenderer")
+                        playlist_render = item.get("playlistRenderer")
+
+                        if video_render and video_render["videoId"]:
+                            items.append(video_render)
+                        if with_playlist and playlist_render and playlist_render["playlistId"]:
+                            items.append({
+                                "id": playlist_render["playlistId"],
+                                "type": "playlist",
+                                "thumbnail": playlist_render["thumbnails"],
+                                "title": playlist_render["title"]["simpleText"],
+                                "length": playlist_render["videoCount"],
+                                "videos": playlist_render["videos"],
+                                "videoCount": playlist_render["videoCount"],
+                                "isLive": False
+                            })
+
+        api_token = page["apiToken"]
+        context = page["context"]
+        next_page_context = {"context": context, "continuation": cont_token}
+        items_result = items[:limit] if limit != 0 else items
+
+        return {"items": items_result, "nextPage": {"nextPageToken": api_token, "nextPageContext": next_page_context}}
+    except Exception as ex:
+        print(ex)
+        return {"error": str(ex)}
+
+def search_and_get_first_video_url(query):
+    data = get_data(query, limit=1)
+    if "items" in data and data["items"]:
+        first_video_id = data["items"][0].get("videoId", None)
+        if first_video_id:
+            return f"{YOUTUBE_ENDPOINT}/watch?v={first_video_id}"
+    return None
+
+def download_mp3(url):
     if not os.path.exists("songs/"):
         os.makedirs("songs/")
-    url = form_youtube_url(video_id)
 
     ydl_opts = {
         'format': 'bestaudio/best',
@@ -123,7 +177,6 @@ def download_mp3(video_id):
             print(f'{info_dict["title"]} has been successfully downloaded as an MP3.')
         else:
             print('ERROR: Failed to download the video as an MP3.')
-        
 #endregion
 
 def main():
@@ -134,15 +187,13 @@ def main():
     #DeezerMP3Backup
     json_playlist = playlist_to_json_file(OUTPUT_FILE, api_url, data)
     
-    # YouTube API
-    api_key = get_creds()
+    # YouTube Download
     for item in json_playlist:
         song_name = item["song"]
-        video_ids = get_video_listings(api_key, song_name)
+        video_query_url = search_and_get_first_video_url(song_name)
 
-        if video_ids:
-            for video_id in tqdm(video_ids):
-                download_mp3(video_id)
+        if video_query_url:
+            download_mp3(video_query_url)
         else:
             print(f'No video IDs found for song: {song_name}')
     return
